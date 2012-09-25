b9635e6c8b5350016d7d4f83144d356ccf20784e
Aaron <aaron@amul.kodeplay.com>
2012-09-25 12:50:00 +0530

    atm puzzle

---
+++ ATM/test.php
@@ -0,0 +1,29 @@
+<?php
+
+//URL will be : http://projects.kp/test.php?wm=115&total=120
+
+$wm = (int)$_GET["wm"]; //Withdrawal amount
+
+$total = (float)$_GET["total"]; //balance
+
+if($wm%5==0){
+
+	if($wm<$total){
+
+		$total = $total - $wm - 0.50;
+		echo "Successful Transaction";
+
+	}else{
+
+		echo "Insufficient Funds";
+
+	}
+
+}else{
+	echo " Incorrect Withdrawal Amount (not multiple of 5)";
+}
+
+echo "</br>";
+echo $total;
+
+?> 
---
+++ ATM/test.py
@@ -0,0 +1,14 @@
+a = 10 #Withdrawal amount
+
+b = 120 #balance
+
+if a%5 == 0:
+	if a < b :
+		b = b - a - 0.50
+		print "Successful Transaction"
+	else :
+		print "Insufficient Funds"
+else:
+	print " Incorrect Withdrawal Amount (not multiple of 5)"
+
+print b
